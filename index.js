19bf41b3608d702a5fa3cf42e0c646b163fc6e05
Stephen Hicks <shicks@users.noreply.github.com>
2017-10-16 17:03:03 -0700

    Update index.js
    Add "play time" as possible filter.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fetch(`https://www.boardgamegeek.com/xmlapi2/thing?id=${id}`).then(
 
 // Ideas:
 //  (1) Read entire collection, query for game info (maybe cache to localstorage to save time? refresh button?)
-//  (2) Provide filters for mechanic, theme, player count (best vs possible), age, rating (personal vs community)
+//  (2) Provide filters for mechanic, theme, player count (best vs possible), age, rating (personal vs community), play time
 //  (3) Maybe expand metadata with a personal dictionary
 //       - BGG allows specifying comments in individual games in collection - support TAG= syntax?
 //         Allow overriding values, such as MAXPLAYERS=5 for The Birds Told Me To Do It?
